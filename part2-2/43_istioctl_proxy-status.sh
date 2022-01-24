308897fcde8a7775a6a6171275388c41db785afc
JustinMacII <justinpark@JustinMacIIui-MacBookPro.local>
2022-01-24 19:48:56 +0900

    .

--- part2-2/43_istioctl_proxy-status.sh
+++ part2-2/43_istioctl_proxy-status.sh
@@ -1,9 +1,9 @@
 #프록시 상태 확인
 # 종류 
-#   CDS : cluster
-#   EDS : endpoint
-#   LDS : listener
-#   RDS : Router
+#   CDS : cluster Discovery Service
+#   EDS : EndPoint Discovery Service
+#   LDS : listener Discovery Service
+#   RDS : Route Discovery Service
 #상태 
 # SYNCED : 동기화 완료
 # STALE : 오래된 상태(상태 오래됨 - 네트워크 등에 문제가 있음)
